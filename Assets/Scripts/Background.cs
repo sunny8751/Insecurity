6e9bf7e6442992d5ea8c07645e32dfc2b5cadce6
Sunwoo Yim <sunny8751@gmail.com>
2015-03-07 13:51:10 -0500

    Added basic stuff.

---
+++ Assets/Scripts/Background.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Background : MonoBehaviour {
+
+	float interval;
+	GameObject [] backgrounds;
+
+	void Start () {
+		backgrounds = GameObject.FindGameObjectsWithTag("Background");
+		interval = backgrounds[0].GetComponent<SpriteRenderer>().sprite.bounds.size.x;
+		backgrounds[1].transform.position = new Vector3(interval, 0,0);
+	}
+	
+	void Update () {
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	float speed = 3f, jumpSpeed = 400f;
+
+	void Start () {
+	
+	}
+	
+	void Update () {
+		movement();
+	}
+
+	void movement(){
+		// left and right movement
+		if(Input.GetKey(KeyCode.RightArrow)){
+			transform.transform.position += Vector3.right*speed*Time.deltaTime;
+		} else if(Input.GetKey(KeyCode.LeftArrow)){
+			transform.transform.position += Vector3.left*speed*Time.deltaTime;
+		}
+		// player wants to jump
+		if(Input.GetKeyDown(KeyCode.Space)){
+			//check to see if the player is on the ground
+			//use a raycast
+
+			rigidbody2D.AddForce(Vector2.up*jumpSpeed);
+		}
+	}
+}
